d334526e994a59ce6280685e2261a40354020e95
jaeyong <maxwodyd98@gmail.com>
2020-04-22 18:29:48 +0900

    1주차 세미나 과제 업로드

---
+++ iOS-firstweek_seminar_assignment/iOS-firstweek_seminar_assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-firstweek_seminar_assignment
+//
+//  Created by 이재용 on 2020/04/22.
+//  Copyright © 2020 jaeyong Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
