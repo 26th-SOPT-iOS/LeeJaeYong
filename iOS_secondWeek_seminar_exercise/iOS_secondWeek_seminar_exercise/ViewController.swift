4c98faf0b7a194812900dc53010d8528fcd28f61
jaeyong <maxwodyd98@gmail.com>
2020-04-25 17:40:37 +0900

    2주차 세미나 실습 업로드

---
+++ iOS_secondWeek_seminar_exercise/iOS_secondWeek_seminar_exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_secondWeek_seminar_exercise
+//
+//  Created by 이재용 on 2020/04/25.
+//  Copyright © 2020 jaeyong Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
